dc314b042eac2d604b453ce5c649bba050bae8d0
Maris Baier <maris.baier@physik.hu-berlin.de>
2024-09-18 14:39:25 +0200

    Append LinearSystemTransformationSlide to config.py

--- config.py
+++ config.py
@@ -14,3 +14,23 @@ class LinearTransformationSlide(Slide, LinearTransformationScene):
 
 class ThreeDSlide(Slide, ThreeDScene):
     pass
+
+class LinearSystemTransformationSlide(LinearTransformationSlide):
+    def setup(self):
+        LinearTransformationSlide.setup(self)
+        equation = VGroup(*[
+            Tex("A"),
+            Tex(r"$\vec{\textbf{x}}$"),
+            Tex("="),
+            Tex(r"$\vec{\textbf{b}}$"),
+        ])
+        equation.scale(1.5)
+        self.equation = equation
+        self.A, self.x, eq, self.v = equation.split()
+        self.x.set_color(PINK)
+        self.v.set_color(YELLOW)
+        equa = VGroup(*[self.A,self.x,eq,self.v])
+        equa.arrange(buff=0.1)
+        equa.next_to(ORIGIN, LEFT).to_edge(UP)
+        equa.add_background_rectangle()
+        self.add_foreground_mobject(equa)
